cfa707398894068fdbd38e4c664abe1e4697b58c
yitzchak shaish <y350380350@gmail.com>
2025-07-16 17:01:47 +0300

    feat: initialize project with user management functionality
    - Added package.json with dependencies for dotenv, express, and mongodb.
    - Created usersCtrl.js for handling user-related operations including login, user creation, retrieval, update, and deletion.
    - Implemented usersDAL.js for database interactions with MongoDB, including functions for user retrieval, insertion, updating, and deletion.
    - Set up usersR.js for defining user-related routes in the Express application.

---
+++ db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+import { config } from 'dotenv';
+
+config();
+console.log(process.env.URI);
+
+const client = new MongoClient(process.env.URI);
+let db;
+
+
+
+export async function connectToDatabase() {
+    try {
+        if (db) {
+            console.log('Connecting to MongoDB...')
+            return db
+        }
+        await client.connect();
+        db = client.db('myDatabase');
+        console.log('Connected to MongoDB');
+        return db;
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+// await client.connect();
+
+//await client.close();
+
+// const bb = await connectToDatabase()
+ //connectToDatabase()
+// const usersCollection =await bb.collection('users');
+
+// await usersCollection.insertOne({ name: "David", email: "david@example.com" });
---
+++ usersDAL.js
@@ -0,0 +1,140 @@
+import { connectToDatabase } from "./db.js";
+import  {ObjectId} from "mongodb";
+
+// export async function getUserById(id) {
+//     const {data, error} = await supabase.from('users').select('*').eq('id', id).single();
+//     if(error) throw error
+//     return data
+
+// }
+
+async function connectToUsersCollection() {
+    const db = await connectToDatabase();
+    return await db.collection('users');
+}
+
+export async function getUserByIdFDB(id) {
+     try {
+        const usersCollection = await connectToUsersCollection();
+        const result = await usersCollection.findOne({"_id":new ObjectId(id)})
+
+        if (!result) {
+            return { success: false, data: null, error: 'user not found' };
+        }
+
+        return { success: true, data: result, error: null };
+    } catch (error) {
+        return { success: false, data: null, error };
+    }
+ 
+}
+
+export async function getUserByNameAndPassword(username, password) {
+   try {
+        const usersCollection = await connectToUsersCollection();
+        const result = await usersCollection.findOne({"username": username, "password": password})
+
+        if (!result) {
+            return { success: false, data: null, error: 'user not found' };
+        }
+
+        return { success: true, data: result, error: null };
+    } catch (error) {
+        return { success: false, data: null, error };
+    }
+}
+
+export async function getAllUsersFDB() {
+    try {
+        const usersCollection = await connectToUsersCollection();
+        const result = await usersCollection.find().toArray();
+
+        if (!result || result.length === 0) {
+            return { success: false, data: null, error: 'No users found' };
+        }
+
+        return { success: true, data: result, error: null };
+    } catch (error) {
+        console.log(`error: concted to mongodb`);
+        
+        return { success: false, data: null, error };
+    }
+}
+
+
+
+export async function insertNewUsernameTDB(username) {
+    const usersCollection = await connectToUsersCollection();
+
+    try {
+        const result = await usersCollection.insertOne(username);
+
+        if (!result.acknowledged) {
+            return { success: false, data: null, error: 'Insert failed' };
+        }
+
+        return { success: true, data: result.insertedId, error: null };
+    } catch (error) {
+        return { success: false, data: null, error };
+    }
+}
+
+
+
+export async function updateUsernameTDB(id, username) {
+    try {
+        const usersCollection = await connectToUsersCollection();
+
+        const result = await usersCollection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set:  username  }
+        );
+console.log(result);
+
+        if (result.modifiedCount === 0) {
+            return { success: false, error: 'No user updated' };
+        }
+
+        return { success: true, error: null };
+
+    } catch (error) {
+        return { success: false, error };
+    }
+}
+
+
+export async function deleteUsernameFDB(id) {
+     try {
+        const usersCollection = await connectToUsersCollection();
+
+        const result = await usersCollection.deleteOne(
+            { _id: new ObjectId(id) }
+        );
+
+        if (result.modifiedCount === 0) {
+            return { success: false, error: 'No user deleted' };
+        }
+
+        return { success: true, error: null };
+
+    } catch (error) {
+        return { success: false, error };
+    }
+}
+
+// const result = await getUserById(1);
+
+// if (!result.success) {
+//     console.error('Error:', result.error.message);
+// } else {
+//     console.log('User:', result.data);
+// }
+
+// const result = await getAllUsers();
+
+// if (!result.success) {
+//     console.error('Insert failed:', result.error.message);
+// } else {
+//     console.log('ss', result.data);
+// }
+
